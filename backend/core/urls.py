7ff9040b5fabd0f6f396ba921a3446f9df1cb293
Renildo15 <renildorabi@ufrn.edu.br>
2023-07-28 19:28:16 -0300

    feat: criando urls do user #12

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views.grammar_views import grammar_list, grammar_detail
+from .views.user_views import user_list, user_detail
 
 urlpatterns = [
     path('grammar/', grammar_list, name="grammar_list_create"),
-    path('grammar/<int:pk>/', grammar_detail, name="grammar_detail")
+    path('grammar/<int:pk>/', grammar_detail, name="grammar_detail"),
+
+    path('user/', user_list, name='user_list_create'),
+    path('user/<int:pk>/', user_detail, name="user_detail")
 ]
