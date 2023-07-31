d1e85c3e4632d908c04622061b81b887c51fa5ec
Renildo15 <renildorabi@ufrn.edu.br>
2023-07-30 21:02:15 -0300

    feat: criando serializer #13

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from core.models import Grammar, Profile
+from core.models import Grammar, Profile, PracticeGrammar
 import re
 
 
@@ -84,3 +84,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['id', 'user', 'phone', 'date_of_birth', 'avatar', 'date_created']
+
+class PracticeGrammarSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PracticeGrammar
+        fields = ['id', 'grammar', 'first_setence', 'second_setence', 'third_setence', 'created_by', 'created_at', 'updated_at']
