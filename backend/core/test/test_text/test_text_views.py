e280fc3bbc6b1b21f8bcfbe9a9e43e64ac5be55a
Renildo15 <renildorabi@ufrn.edu.br>
2023-08-18 10:51:10 -0300

    test: add testes para texts views #28

---
+++ backend/core/test/test_text/test_text_views.py
@@ -0,0 +1,111 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from core.models import Text
+from django.contrib.auth.models import User
+
+
+class TextTestsView(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.text = Text.objects.create(
+            title='神のイタズラ',
+            text = '''
+                    ある日、神様が、人間の世界を見ていました。
+                    すると、人間たちが、いろいろなことで、争っているのが見えました。
+                    神様は、人間たちに、争いをやめるように、言いました。
+                ''',
+            annotation = 'teste',
+            created_by=self.user
+        )
+
+        self.client = APIClient()
+    
+    def test_text_list(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get('/api/text/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_text_create(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post('/api/text/', {
+            'title': '神のイタズラ',
+            'text': '''
+                    ある日、神様が、人間の世界を見ていました。
+                    すると、人間たちが、いろいろなことで、争っているのが見えました。
+                    神様は、人間たちに、争いをやめるように、言いました。
+                ''',
+            'annotation': 'teste',
+            'created_by': self.user.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_text_create_invalid(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post('/api/text/', {
+            'title': '',
+            'text': '',
+            'annotation': '',
+            'created_by': self.user.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_text_detail(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(f'/api/text/{self.text.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_text_detail_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(f'/api/text/{999}/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_text_update(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(f'/api/text/{self.text.id}/', {
+            'title': '神のイタズラ',
+            'text': '''
+                    ある日、神様が、人間の世界を見ていました。
+                    すると、人間たちが、いろいろなことで、争っているのが見えました。
+                    神様は、人間たちに、争いをやめるように、言いました。
+                ''',
+            'annotation': 'teste',
+            'created_by': self.user.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_text_update_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(f'/api/text/{999}/', {
+            'title': '神のイタズラ',
+            'text': '''
+                    ある日、神様が、人間の世界を見ていました。
+                    すると、人間たちが、いろいろなことで、争っているのが見えました。
+                    神様は、人間たちに、争いをやめるように、言いました。
+                ''',
+            'annotation': 'teste',
+            'created_by': self.user.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_text_update_invalid(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(f'/api/text/{self.text.id}/', {
+            'title': '',
+            'text': '',
+            'annotation': '',
+            'created_by': self.user.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_text_delete(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/api/text/{self.text.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_text_delete_not_found(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/api/text/{999}/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
