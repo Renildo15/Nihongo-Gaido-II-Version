9c5ecb6581c155aa7855f81e29d6f4643055d730
Renildo15 <renildorabi@ufrn.edu.br>
2023-09-08 18:18:51 -0300

    feat: criando tela de login #33

--- frontend/nihongo-gaido/pages/login/index.tsx
+++ frontend/nihongo-gaido/pages/login/index.tsx
@@ -4,9 +4,7 @@ import { Button,
         VStack, 
         Center,
         FormControl, 
-        Input, 
-        Heading, 
-        Text,
+        Input,
         InputInput,
         InputIcon,
         Icon,
@@ -20,6 +18,7 @@ import Logo from "../../public/logo.png";
 
 export default function Login() {
     const [showPassword, setShowPassword] = useState(false)
+
     const handleState = () => {
         setShowPassword((showState) => {
         return !showState
@@ -27,42 +26,58 @@ export default function Login() {
     }
     
     return (
-        <Box h={600} borderWidth={1} alignItems="center" justifyContent="center">
-            <FormControl>
-                <VStack space="xl">
-                    <Image
-                        src={Logo}
-                        alt="Logo Teze"
-                        width={150}
-                        height={150}
-                        priority
-                    />
-                </VStack>
-                <VStack space="xl" w={900} h={400}>
-                    <VStack space="xs">
-                        <Input>
-                            <InputInput type="text" placeholder="Username" color="grey" />
-                        </Input>
+       <Center>
+            <Box display="flex" justifyContent="center" alignItems="center" flexDirection="colunm">
+                <FormControl mt="130px">
+                    <VStack space="md" justifyContent="center" alignItems="center" mb="20px">
+                        <Image
+                            src={Logo}
+                            alt="Logo Teze"
+                            width={150}
+                            height={150}
+                            priority
+                        />
                     </VStack>
-                    <VStack space="xs">
-                        <Input>
-                            <InputInput type={showPassword ? "text" : "password"} placeholder="Senha" color="grey" />
-                            <InputIcon pr="$3" onPress={handleState}>
-                        
-                            <Icon
-                                as={showPassword ? EyeIcon : EyeOffIcon}
-                                color="$darkBlue500"
-                            />
-                            </InputIcon>
-                        </Input>
+                    <VStack space="md" width="400px">
+                        <VStack space="md" mb="10px">
+                            <Input>
+                                <InputInput type="text" placeholder="Username" color="grey" />
+                            </Input>
+                        </VStack>
+                        <VStack space="md" mb="10px">
+                            <Input>
+                                <InputInput type={showPassword ? "text" : "password"} placeholder="Senha" color="grey" />
+                                <InputIcon pr="$3" onPress={handleState}>
+                            
+                                <Icon
+                                    as={showPassword ? EyeIcon : EyeOffIcon}
+                                    color="$darkBlue500"
+                                />
+                                </InputIcon>
+                            </Input>
+                        </VStack>
+                        <Button
+                            ml="auto"
+                            bg="$red600"
+                            sx={{
+                                ":hover": {
+                                  bg: "$red700",
+                                },
+                                ":active": {
+                                  bg: "$red800",
+                                },
+                              }}
+                        >
+                        <ButtonText 
+                            color="$white"
+                          
+                        >
+                            Login
+                        </ButtonText>
+                        </Button>
                     </VStack>
-                    <Button
-                        ml="auto"
-                    >
-                    <ButtonText color="$white">Login</ButtonText>
-                    </Button>
-                </VStack>
-            </FormControl>
-        </Box>
+                </FormControl>
+            </Box>
+       </Center>
     )
 }
