e943c20889da69647b8331410d6e3f65be7b5917
Renildo15 <renildorabi@ufrn.edu.br>
2023-09-09 21:11:09 -0300

    feat: criando authentication #34

---
+++ backend/core/utils/authentication.py
@@ -0,0 +1,46 @@
+import jwt
+from rest_framework.authentication import BaseAuthentication
+from django.middleware.csrf import CsrfViewMiddleware
+from rest_framework import exceptions
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class CSRFCheck(CsrfViewMiddleware):
+    def _reject(self, request, reason):
+        return reason
+    
+class SafeJWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        header = request.headers.get("Authorization")
+
+        if not header:
+            return None
+
+        try:
+            access_token = header.split(" ")[1]
+            payload = jwt.decode(access_token, settings.SECRET_KEY, algorithms=["HS256"])
+
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed("access token expired")
+
+        except IndexError:
+            raise exceptions.AuthenticationFailed("Token prefix missing")
+
+        user = User.objects.filter(id=payload["user_id"]).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed("User not found")
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed("User is inactive")
+
+        return (user, None)
+    
+    def enforce_csrf(self, request):
+        check = CSRFCheck()
+        check.process_request(request)
+        reason = check.process_view(request, None, (), {})
+        
+        if reason:
+            raise exceptions.PermissionDenied("CSRF Failed: %s" % reason)
