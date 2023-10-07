8736937ff1691c6740e921100427e4b70a9b2388
Renildo15 <renildorabi@ufrn.edu.br>
2023-10-07 16:27:29 -0300

    feat: verificando se foi alterado #40

--- frontend/components/profile/ModalProfile.tsx
+++ frontend/components/profile/ModalProfile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useContext, useEffect, ChangeEvent} from "react";
 import { 
     Modal, 
     ModalBackdrop, 
@@ -14,29 +14,101 @@ import {
     ModalFooter,
     CloseIcon,
     FormControl,
-    FormControlLabel,
     FormControlLabelText,
     Input,
     InputInput,
     HStack,
+    Box, 
+    Text
 } from "@gluestack-ui/themed";
+
+import { useProfile } from "@/utils/api";
+import {AuthContext} from "@/context/AuthContext";
+import InputMask from 'react-input-mask';
 
 
 interface ModalProfileProps {
     isOpen: boolean;
     onClose: () => void;
-    ref?: any;
 }
 
-export default function ModalProfile({ isOpen, onClose, ref }: ModalProfileProps) {
+export default function ModalProfile({ isOpen, onClose }: ModalProfileProps) {
+
+    
+
+    const {userInfo} = useContext(AuthContext)
+
+    const {
+        data: originalProfile,
+        error: originalProfileError,
+        mutate: originalProfileMutate,
+    } = useProfile(userInfo?.id)
+
+    const [nome, setNome] = useState(originalProfile?.user.first_name || "")
+    const [sobrenome, setSobrenome] = useState(originalProfile?.user.last_name || "")
+    const [username, setUsername] = useState(originalProfile?.user.username || "")
+    const [email, setEmail] = useState(originalProfile?.user.email || "")
+    const [telefone, setTelefone] = useState(originalProfile?.phone || "")
+    const [dataNascimento, setDataNascimento] = useState(originalProfile?.date_of_birth || "")
+
+    const [telefoneValido, setTelefoneValido] = useState(true)
+
+    
+    const someInfoChanged = (
+        nome !== originalProfile?.user.first_name ||
+        sobrenome !== originalProfile?.user.last_name ||
+        username !== originalProfile?.user.username ||
+        email !== originalProfile?.user.email ||
+        telefone !== originalProfile?.phone ||
+        dataNascimento !== originalProfile?.date_of_birth
+    )
+
+    function setOriginalValues(){
+        if(originalProfile){
+            setNome(originalProfile?.user.first_name)
+            setSobrenome(originalProfile?.user.last_name)
+            setUsername(originalProfile?.user.username)
+            setEmail(originalProfile?.user.email)
+            setTelefone(originalProfile?.phone)
+            setDataNascimento(originalProfile?.date_of_birth)
+        }
+    }
+
+
+    const isValidPhone = (phone: string) => {
+        const regex = /^\(\d{2}\) \d \d{4}-\d{4}$/;
+        return regex.test(phone);
+    }
+
+    const handleTelefoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+    
+        setTelefone(value);
+    
+        if (!isValidPhone(value)) {
+            setTelefoneValido(false);
+        } else {
+            setTelefoneValido(true);
+        }
+    }
+    
+
+    if(originalProfileError) {
+        return (
+            <Box justifyContent="center" alignItems="center" w={"100%"} >
+                <Text color="#D02C23">Erro ao carregar perfil</Text>
+            </Box>
+        )
+    }
+
+
     return(
         <Modal
             isOpen={isOpen}
             onClose={onClose}
-            finalFocusRef={ref}
         >
             <ModalBackdrop />
-            <ModalContent maxWidth="550px">
+            <ModalContent maxWidth={550}>
                 <ModalHeader>
                     <Heading color="#D02C23" size="lg">Editar perfil</Heading>
                     <ModalCloseButton>
@@ -49,30 +121,53 @@ export default function ModalProfile({ isOpen, onClose, ref }: ModalProfileProps
                             <VStack space="md">
                                 <FormControlLabelText color="#D02C23">Nome:</FormControlLabelText>
                                 <Input>
-                                    <InputInput />
+                                    <InputInput
+                                        value={nome}
+                                        onChange={(e) => setNome(e.target.value)}
+                                    />
                                 </Input>
                                 <FormControlLabelText color="#D02C23">Sobrenome:</FormControlLabelText>
                                 <Input>
-                                    <InputInput />
+                                    <InputInput 
+                                        value={sobrenome}
+                                        onChange={(e) => setSobrenome(e.target.value)}
+                                    />
                                 </Input>
                                 <FormControlLabelText color="#D02C23">Username:</FormControlLabelText>
                                 <Input>
-                                    <InputInput />
+                                    <InputInput 
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                    />
                                 </Input>
                             
                             </VStack>
                             <VStack space="md">
                                 <FormControlLabelText color="#D02C23">Email:</FormControlLabelText>
                                     <Input>
-                                        <InputInput />
+                                        <InputInput 
+                                            value={email}
+                                            onChangeText={setEmail}
+                                        />
                                     </Input>
                                     <FormControlLabelText color="#D02C23">Telefone:</FormControlLabelText>
                                     <Input>
-                                        <InputInput />
+                                        <InputMask
+                                            mask="(99) 9 9999-9999"
+                                            value={telefone}
+                                            onChange={handleTelefoneChange}
+                                            
+                                        >
+                                            {(inputProps) => <InputInput {...inputProps} />}
+                                        </InputMask>                                     
                                     </Input>
+                                    {!telefoneValido && <div style={{color: 'red'}}>Número de telefone inválido.</div>}
                                     <FormControlLabelText color="#D02C23">Data de Nascimento:</FormControlLabelText>
                                     <Input>
-                                        <InputInput />
+                                        <InputInput 
+                                            value={dataNascimento}
+                                            onChange={(e) => setDataNascimento(e.target.value)}
+                                        />
                                     </Input>
                             </VStack>
                         </HStack>
@@ -84,7 +179,11 @@ export default function ModalProfile({ isOpen, onClose, ref }: ModalProfileProps
                         size="sm"
                         action="secondary"
                         mr="$3"
-                        onPress={onClose}
+                        onPress={() =>{
+                            setOriginalValues()
+                            onClose()
+                        }}
+                        isDisabled={!someInfoChanged}
                     >
                         <ButtonText>Cancel</ButtonText>
                     </Button>
@@ -102,6 +201,7 @@ export default function ModalProfile({ isOpen, onClose, ref }: ModalProfileProps
                               bg: "$red800",
                             },
                         }}
+                        isDisabled={!someInfoChanged}
                     >
                         <ButtonText>Salvar</ButtonText>
                     </Button>
