1ae4f4880b95053f508d20eb9cdc2295fbc92cdc
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-11-05 19:38:06 -0300

    refactor: deletando front #62

--- frontend/components/home/HomeContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, Dispatch, SetStateAction } from "react";
-
-const defaultValue: {
-  menuIsCollapsedState: [boolean, Dispatch<SetStateAction<boolean>>]
-} = {
-  menuIsCollapsedState: [false, () => {}]
-}
-
-export const HomeContext = createContext(defaultValue)
