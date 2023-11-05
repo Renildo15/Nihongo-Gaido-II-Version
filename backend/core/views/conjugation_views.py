5912f42551e6961a635a4250256265fb9e720b2a
Renildo15 <renildorabi@ufrn.edu.br>
2023-11-05 18:46:46 -0300

    fix: corrigindo problema em word #63

--- backend/core/views/conjugation_views.py
+++ backend/core/views/conjugation_views.py
@@ -11,13 +11,14 @@ from core.utils.paginationn import CustomPagination
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
 def conjugation_list(request, word_id):
+    
     try:
         word = Word.objects.get(pk=word_id)
     except Word.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
     if request.method == "GET":
-        conjugation = Conjugation.objects.get(created_by=request.user, word=word)
+        
+        conjugation = Conjugation.objects.filter(created_by=request.user, word=word)
         
         paginator = CustomPagination()  
         result_page = paginator.paginate_queryset(conjugation, request)
@@ -39,7 +40,8 @@ def conjugation_list(request, word_id):
             return Response(data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    else: 
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view(["PATCH", "DELETE"])
 @permission_classes([IsAuthenticated])
