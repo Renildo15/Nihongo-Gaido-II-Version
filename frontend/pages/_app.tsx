e6066c96badc18f1ec5101a18541ef5c95690e16
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-11-06 16:15:23 -0300

    fix: corrigindo front #62

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,19 +2,11 @@ import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NativeBaseProvider } from "native-base";
-import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NativeBaseProvider isSSR>
-      <SWRConfig
-        value={{
-          revalidateOnFocus: false,
-          revalidateOnReconnect: false,
-        }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
+      <Component {...pageProps} />
     </NativeBaseProvider>
   );
 }
