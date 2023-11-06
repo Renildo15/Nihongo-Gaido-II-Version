8a67da3d06afce9c6c346e7d07490fac56cd8dc2
Renildo15 <renildorabi@ufrn.edu.br>
2023-11-06 11:39:08 -0300

    refactor: configurando projeto #62

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NativeBaseProvider } from "native-base";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NativeBaseProvider isSSR>
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          revalidateOnFocus: false,
+          revalidateOnReconnect: false,
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
     </NativeBaseProvider>
   );
 }
