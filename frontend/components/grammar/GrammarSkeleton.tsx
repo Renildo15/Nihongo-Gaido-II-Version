6c56fbee340526e964cbbca001c6be058f2295ed
Renildo15 <renildorabi@ufrn.edu.br>
2023-11-19 09:29:04 -0300

    fera: grammar skeleton #69

---
+++ frontend/components/grammar/GrammarSkeleton.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Column, Skeleton } from "native-base"
+
+export default function GrammarSkeleton() {
+    return (
+        <Column space={2} w={'100%'}>
+            <Skeleton 
+                height={30}
+                startColor={'#D02C23'}
+                endColor={'#F2F2F2'} 
+            />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+            <Skeleton height={30} />
+        </Column> 
+    )
+}
