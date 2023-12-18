df65413bc7efd059d94061e23fe9cc0b274a1c46
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-18 17:47:07 -0300

    feat: skeletons #78

---
+++ frontend/components/sentence/SentenceSkeleton.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Skeleton, Column} from "native-base";
+
+export default function SentenceSkeleton() {
+    return (
+        <Column space={2} w={'100%'} justifyContent={'center'} alignItems={'center'}>
+            <Skeleton w={'50%'} p={'10px'} h={'98px'}/>
+            <Skeleton w={'50%'} p={'10px'} h={'98px'}/>
+            <Skeleton w={'50%'} p={'10px'} h={'98px'}/>
+            <Skeleton w={'50%'} p={'10px'} h={'98px'}/>
+        </Column>
+    )
+}
