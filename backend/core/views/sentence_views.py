71e2800dbe290e27aa8e0ca1b3fcfe8263592228
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-18 18:36:39 -0300

    refactor: alterando view #78

--- backend/core/views/sentence_views.py
+++ backend/core/views/sentence_views.py
@@ -10,18 +10,20 @@ from core.utils.paginationn import CustomPagination
 
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
-def sentences_list(request, grammar_id):
-    try:
-        sentences = Sentence.objects.filter(grammar_id=grammar_id, created_by=request.user)
-    except Sentence.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+def sentences_list(request, grammar_id=None):
+    if grammar_id:
+        try:
+            sentences = Sentence.objects.filter(grammar_id=grammar_id, created_by=request.user)
+        except Sentence.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method == "GET":
-        paginator = CustomPagination()
-        result_page = paginator.paginate_queryset(sentences, request)
-        serializer = SentenceListSerializer(instance=result_page, many=True)
+        if request.method == "GET":
+            paginator = CustomPagination()
+            result_page = paginator.paginate_queryset(sentences, request)
+            serializer = SentenceListSerializer(instance=result_page, many=True)
 
-        return paginator.get_paginated_response(serializer.data)
+            return paginator.get_paginated_response(serializer.data)
+        
     elif request.method == "POST":
         serializer = SentenceCreateSerializer(data=request.data)
 
@@ -33,6 +35,8 @@ def sentences_list(request, grammar_id):
             return Response(data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(["GET", "PATCH", "DELETE"])
