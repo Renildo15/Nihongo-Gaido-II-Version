c74517654e5105c5f5b8a5479a9e221088caa7dc
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-20 18:14:19 -0300

    feat: criando hooks #78

--- frontend/utils/api/sentence.ts
+++ frontend/utils/api/sentence.ts
@@ -25,7 +25,6 @@ export interface ISentenceUpdate {
     sentence?: string;
     translate?: string;
     annotation?: string;
-    grammar?: number;
 }
 
 export function useSentences(grammarId: number | undefined) {
@@ -50,6 +49,28 @@ export function useSentences(grammarId: number | undefined) {
     }
 }
 
+export function useSentence(sentenceId: number | undefined) {
+    interface SentenceReponse {
+        sentence: ISentenceList;
+    }
+
+    const { 
+        data, 
+        error, 
+        isLoading, 
+        isValidating, 
+        mutate
+    } = useSWR<SentenceReponse>(`/api/sentence/${sentenceId}`, fetcchSimple);
+
+    return {
+        data: data?.sentence,
+        error,
+        isLoading,
+        isValidating,
+        mutate
+    }
+}
+
 export async function createSentence({sentence, translate, annotation, grammar, created_by }:ISentenceCreate){
     interface ISentenceResponse{
         message: string
@@ -57,6 +78,19 @@ export async function createSentence({sentence, translate, annotation, grammar, 
 
     try {
         const res = await axios.post<ISentenceResponse>(`/api/sentences/${grammar}`, {sentence, translate, annotation, grammar, created_by})
+        return res.data.message
+    } catch (error: any) {
+        throw new Error(error.message)
+    }
+}
+
+export async function updateSentence(id: number, {sentence, translate, annotation}:ISentenceUpdate){
+    interface ISentenceResponse{
+        message: string
+    }
+
+    try {
+        const res = await axios.patch<ISentenceResponse>(`/api/sentence/${id}`, {sentence, translate, annotation})
         return res.data.message
     } catch (error: any) {
         throw new Error(error.message)
