5a1e6d8f608cd8c6508bd6f6f30337ce222025c1
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-24 16:06:02 -0300

    feat: usando filtros #79

--- frontend/components/vocabulary/VocabularyList.tsx
+++ frontend/components/vocabulary/VocabularyList.tsx
@@ -1,19 +1,60 @@
-import React from "react"
+import React, {useMemo} from "react"
 import { FlatList, Pressable, Text, Divider, Row, Column, Button, Spinner } from "native-base"
 import { ListRenderItemInfo } from "react-native"
 import { useWords, IWordList } from "../../utils/api/vocabulary"
 import { MdList, MdAdd } from "react-icons/md"
 import DataEmpty from "../DataEmpty"
 import Error from "../Error"
+import { IVocabularyFilters } from "./SearchVocabulary"
 
+interface IWordListProps {
+    filters?: IVocabularyFilters
+}
 
-export default function WordList() {
+export default function WordList(props: IWordListProps) {
     const {
         data: words, 
         error: wordsError,
         isLoading: wordsIsLoading,
         isValidating: wordsIsValidating,
     } = useWords()
+
+
+    const filteredVocabulary = useMemo(() => {
+        if(words === undefined) return []
+
+        const filters = props.filters
+
+        if(filters === undefined) return words
+
+        if(Object.values(filters).every(v => v === null)) return words
+
+        let filteredWords = words
+
+        if(filters.searchText !== null) {
+            filteredWords = filteredWords.filter(word => 
+                word.word.toLocaleLowerCase().includes(filters.searchText?.toLocaleLowerCase() ?? "") ||
+                word.reading.toLocaleLowerCase().includes(filters.searchText?.toLocaleLowerCase() ?? "") ||
+                word.meaning.toLocaleLowerCase().includes(filters.searchText?.toLocaleLowerCase() ?? "")
+            )
+        }
+
+        if(filters.category !== null) {
+            filteredWords = filteredWords.filter(word => {
+                return word.category !== null && word.category.name === filters.category;
+            });
+        }
+
+        if(filters.level !== null) {
+            filteredWords = filteredWords.filter(word => word.level === filters.level)
+        }
+
+        if(filters.type !== null) {
+            filteredWords = filteredWords.filter(word => word.type === filters.type)
+        }
+
+        return filteredWords
+    }, [words, props.filters])
 
 
     if (wordsError) return <Error message={wordsError.message} />
@@ -61,7 +102,7 @@ export default function WordList() {
     return (
         <FlatList
             w={"60%"}
-            data={words}
+            data={filteredVocabulary}
             ListHeaderComponent={header}
             renderItem={items}
             keyExtractor={item => item.id.toString()}
--- frontend/pages/vocabulary/index.tsx
+++ frontend/pages/vocabulary/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { GetServerSidePropsContext } from "next";
 import { redirectIfNoCredentials } from "../../utils";
 import { BaseLayout } from "../../components/home/BaseLayout";
 import WordList from "../../components/vocabulary/VocabularyList";
+import SearchVocabulary from "../../components/vocabulary/SearchVocabulary";
 import { Box } from "native-base";
+import { IVocabularyFilters } from "../../components/vocabulary/SearchVocabulary";
 
 export async function getServerSideProps({req, res}: GetServerSidePropsContext) {
     return redirectIfNoCredentials({req, res});
@@ -11,13 +13,15 @@ export async function getServerSideProps({req, res}: GetServerSidePropsContext) 
 
 
 export default function Vocabulary() {
+    const [filters, setFilters] = useState<IVocabularyFilters>()
     return (
         <BaseLayout title="Vocabulary">
+            <SearchVocabulary onFiltersChanged={setFilters}/>
             <Box
                 justifyContent={'center'}
                 alignItems={'center'}
             >
-                <WordList />    
+                <WordList filters={filters} />    
             </Box>
         </BaseLayout>
             
