636318c608a105f40cb7a6ac70544b450212b91e
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-24 16:05:14 -0300

    feat: criando hook do categores #79

--- frontend/utils/api/category.ts
+++ frontend/utils/api/category.ts
@@ -1,4 +1,6 @@
-
+import axios from "axios";
+import { fetcchSimple } from "./user";
+import useSWR from "swr";
 export interface ICategoryList {
     id: number;
     name: string;
@@ -6,4 +8,25 @@ export interface ICategoryList {
     created_by: number;
     created_at: string;
     updated_at: string;
+}
+
+export function useCategories() {
+    interface CategoriesResponse {
+        results: ICategoryList[];
+    }
+
+    const { 
+        data, 
+        error, 
+        isLoading, 
+        isValidating, 
+        mutate
+    } = useSWR<CategoriesResponse>("/api/categories", fetcchSimple);
+    return {
+        data: data?.results,
+        error,
+        isLoading,
+        isValidating,
+        mutate
+    }
 }
