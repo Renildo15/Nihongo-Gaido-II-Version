8b2ad0bd120b3fd4a251e1f1037d635ca5c454d1
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-24 16:05:42 -0300

    feat: criando filtros #79

---
+++ frontend/components/vocabulary/FilterByCategory.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { Select } from "native-base"
+import { useCategories } from "../../utils/api/category"
+import { useWords } from "../../utils/api/vocabulary"
+
+interface IFilterByCategoryProps {
+    onCategorySelected?: (selectedCategory: string | null) => void;
+}
+
+export default function FilterByCategory(props: IFilterByCategoryProps) {
+    const [selectedCategory, setSelectedCategory] = React.useState<string>()
+
+    const {
+        data: categories,
+        error: categoriesError,
+        isLoading: categoriesIsLoading
+    } = useCategories()
+
+    const {
+        data: words,
+        error: wordsError,
+        isLoading: wordsIsLoading
+    } = useWords()
+
+    const handleSelectCategory = (category: string) => {
+        setSelectedCategory(category)
+
+        const selectedCategory = category === "category" ? null : category
+
+        if (props.onCategorySelected !== undefined) {
+            props.onCategorySelected(selectedCategory)
+        }
+    }
+
+    return (
+        <Select
+            isDisabled={!categories || categoriesError !== undefined || categoriesIsLoading || !words || wordsError !== undefined || wordsIsLoading}
+            size={'md'}
+            _light={{
+                bg: 'white'
+            }}
+            _dark={{
+                bg: '#262626'
+            }}
+            selectedValue={selectedCategory}
+            onValueChange={handleSelectCategory}
+            minWidth={200}
+            shadow={1}
+            accessibilityLabel="Select category"
+            placeholder="Select category"
+            mt={1}
+            w={'200px'}
+            _selectedItem={{
+                w: "20%"
+            }}
+        >
+            <Select.Item label="Category" value="category" />
+            {categories?.map((category, index) => (
+                <Select.Item label={category.name} value={category.name} key={index} />
+            )) ?? []}
+        </Select>
+    )
+}
---
+++ frontend/components/vocabulary/SearchVocabulary.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Row, Column, Input, Button } from "native-base";
+import FilterByCategory from "./FilterByCategory";
+import FilterByLevel from "../grammar/FilterByLevel";
+import FilterByType from "./FilterByType";
+import { MdSearch, MdAdd } from "react-icons/md";
+
+export interface IVocabularyFilters {
+    searchText: string | null
+    category: string | null
+    level: string | null
+    type: string | null
+}
+
+interface ISearchVocabularyProps {
+    onFiltersChanged?: (filters: IVocabularyFilters) => void
+}
+
+export default function SearchVocabulary (props: ISearchVocabularyProps) {
+    const [filters, setFilters] = useState<IVocabularyFilters>({
+        searchText: null,
+        category: null,
+        level: null,
+        type: null
+    })
+
+    function handleFilterChanged(filters: IVocabularyFilters) {
+        setFilters(filters)
+
+        if (props.onFiltersChanged !== undefined) {
+            props.onFiltersChanged(filters)
+        }
+    }
+
+    return (
+        <Row justifyContent={'space-between'} p={5}>
+            <Column space={'20px'}> 
+                <Input
+                    placeholder="Search word, reading and meaning"
+                    _light={{
+                        bg: 'white'
+                    }}
+                    _dark={{
+                        bg: '#262626'
+                    }}
+                    w={'700px'}
+                    size={'md'}
+                    onChangeText={text => handleFilterChanged({...filters, searchText: text})}
+                    value={filters.searchText ?? ""}
+                    InputRightElement={<MdSearch size={25} color="#D02C23" />}
+                />
+                <Column width={'200px'}>
+                    <FilterByCategory
+                        onCategorySelected={category => handleFilterChanged({...filters, category})}
+                    />
+                </Column>
+            </Column>
+            <Column space={'20px'} alignItems={'flex-end'}>
+                <FilterByLevel
+                    onLevelSelected={level => handleFilterChanged({...filters, level})}
+                />
+                <FilterByType
+                    onTypeSelected={type => handleFilterChanged({...filters, type})}
+                />
+            </Column>
+        </Row>
+    )
+}
