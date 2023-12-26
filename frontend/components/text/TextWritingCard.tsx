e13c5abe8e0621f29377d4fd52fbc896635d3dda
Renildo15 <renildorabi@ufrn.edu.br>
2023-12-26 17:59:00 -0300

    feat: TextWritingCard #80

---
+++ frontend/components/text/TextWritingCard.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { MdWrapText } from "react-icons/md"
+import { Column, Pressable, Text, Box } from "native-base"
+
+export default function TextWritingCard() {
+    return (
+        <Pressable
+            p={'10px'}
+            _hover={{
+                bg: 'gray.500'
+            
+            
+            }}
+            flexDirection={'column'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            rounded={'md'}
+            _dark={{
+                borderColor: 'gray.700',
+                bg: 'gray.700',
+            }}
+            _light={{
+                borderColor: 'gray.100',
+                bg: 'gray.100',
+            }}
+            h={'200px'}
+            w={'200px'}
+        >
+            <Column
+                space={2}
+            >
+                <Box
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                >
+                    <MdWrapText size={50} color={'white'}/>
+                </Box>
+                <Text
+                    textAlign={'center'}
+                    fontSize={'xl'}
+                    fontWeight={'bold'}
+                >
+                    Writing
+                </Text>
+                <Text
+                    textAlign={'justify'}
+                    fontSize={'sm'}
+                    fontWeight={'400'}
+                >
+                    Here you can practice your writing skills.
+                </Text>
+            </Column>
+        </Pressable>
+    )
+}
