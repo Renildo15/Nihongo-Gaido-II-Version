736431b3bd95d95b4765a2c8e211d5bce7ab9a94
Renildo Rabil <renildorabi@ufrn.edu.br>
2024-01-01 11:55:00 -0300

    refactor: refatorando componentes #80

--- frontend/components/text/TextTranslate/TextTranslateDetail.tsx
+++ frontend/components/text/TextTranslate/TextTranslateDetail.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
-import { Text, Column, Row, Button } from "native-base"
-import { useText } from "../../../utils/api/text"
-// @ts-ignore
-import HTMLRenderer from 'react-html-renderer'
-import Error from "../../Error"
-import ModalDeleteText from "./ModalDeleteText"
+import { Column } from "native-base"
+import TranslateView from "./TranslateView"
+import TranslateUpdate from "./TranslateUpdate"
+
+enum TextTranslateComponent {
+    TranslateView,
+    TranslateUpdate
+}
 
 interface ITextDetailProps {
     textId: number
@@ -12,121 +14,31 @@ interface ITextDetailProps {
 
 export default function TextTranslateDetail(props: ITextDetailProps) {
 
-    const [modalDeleteText, setModalDeleteText] = useState<boolean>(false)
+    const [currentComponent, setCurrentComponent] = useState<TextTranslateComponent>(TextTranslateComponent.TranslateView)
 
-
-
-    const {
-        data: text,
-        error: textError,
-        isLoading: textLoading,
-    } = useText(props.textId)
-
-    if (textError) {
-        return <Error message={textError.message} />
-    }
-
-    if (textLoading) {
-        return <Text>Loading...</Text>
-    }
-
-    return (
-        <Column 
-        width={'100%'}  
-        px={3} 
-        py={2}
-        space={4}
-    >
-            <Row
-                justifyContent={'space-around'}
-                alignItems={'flex-start'}
-                mt={'10px'}
-            >
-                <Column 
-                    space={4}
-                    _dark={{
-                        bg: 'gray.700',
-                    }}
-                    _light={{
-                        bg: 'white',
-                    }}
-                    p={"10px"}
-                    rounded={'md'}
-                    w={'45%'}
-                >
-                    <Text fontWeight={600} fontSize={15} textAlign={'center'}>Original</Text>
-                    <HTMLRenderer html={text?.text} components={{ h1: Paragraph, p :Paragraph }} />
-                </Column>
-
-                <Column 
-                    space={4}
-                    _dark={{
-                        bg: 'gray.700',
-                    }}
-                    _light={{
-                        bg: 'white',
-                    }}
-                    p={"10px"}
-                    rounded={'md'}
-                    w={'45%'}
-                >
-                    <Text fontWeight={600} fontSize={15} textAlign={'center'}>Translate</Text>
-                    <HTMLRenderer html={text?.translate ??  '<p>oba</p>'} components={{ h1: Paragraph, p :Paragraph }} />
-                </Column>
-            </Row>
-            <Row
-                justifyContent={'space-around'}
-                alignItems={'flex-start'}
-                mt={'10px'}
-            >
-                <Column
-                    space={4}
-                    _dark={{
-                        bg: 'gray.700',
-                    }}
-                    _light={{
-                        bg: 'white',
-                    }}
-                    p={"10px"}
-                    rounded={'md'}
-                    w={'45%'}
-                >   
-                    <Text fontWeight={600} fontSize={15} textAlign={'center'}>Annotation</Text>
-                    <HTMLRenderer  html={text?.annotation || '<p>Nenhuma anotação disponível</p>'}components={{ h1: Paragraph, p :Paragraph }} />
-                    <Row
-                        justifyContent={'space-around'}
-                        alignItems={'flex-start'}
-                        mt={'10px'}
+    function getTextTranslateComponent() {
+        switch (currentComponent) {
+            case TextTranslateComponent.TranslateView:
+                return <TranslateView textId={props.textId} onEdit={() => setCurrentComponent(TextTranslateComponent.TranslateUpdate)}/>
+                        
+            case TextTranslateComponent.TranslateUpdate:
+                return (
+                    <Column
+                        justifyContent={'center'}
+                        alignItems={'center'}
                     >
-                        <Button
-                            bg={'#D02C23'}
-                            _hover={{bg: '#ae251e'}}
-                            _pressed={{bg: '#ae251e'}}
-                        >
-                            Edit text
-                        </Button>
-                        <Button
-                            variant={'ghost'}
-                            colorScheme={'red'}
-                            onPress={() => setModalDeleteText(true)}
-                        >
-                            Delete text
-                        </Button>
-                    </Row>
-                </Column>
-            </Row>
+                        <TranslateUpdate
+                            textId={props.textId}
+                            onBack={() => setCurrentComponent(TextTranslateComponent.TranslateView)}
+                        />
+                    </Column>
 
-            <ModalDeleteText
-                isOpen={modalDeleteText}
-                onClose={() => setModalDeleteText(false)}
-                textId={props.textId}
-            />
-        </Column>
-    )
-}
-
-function Paragraph(props: any) {
+                )
+        }
+    }
     return (
-        <Text fontSize={16} fontWeight={500}>{props.children}</Text>
+        <>
+            {getTextTranslateComponent()}
+        </>
     )
 }
