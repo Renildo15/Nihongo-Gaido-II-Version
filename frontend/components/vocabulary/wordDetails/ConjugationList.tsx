8fa5b4fd06c3f8b534c33bc4b32bbe37580c3ad6
Renildo Rabil <renildorabi@ufrn.edu.br>
2024-01-20 10:51:52 -0300

    feat: usando modais #84

--- frontend/components/vocabulary/wordDetails/ConjugationList.tsx
+++ frontend/components/vocabulary/wordDetails/ConjugationList.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { Row, Text, Column } from "native-base";
+import React, { useState } from "react";
+import { Row, Text, Column, Pressable } from "native-base";
 import { useConjugations} from "../../../utils/api/conjugation";
-
+import { MdEdit, MdDelete } from "react-icons/md";
+import ModalDeleteConjugation from "./ModalDeleteConjugation";
+import ModalUpdateConjugation from "./ModalUpdateConjugation";
+import DataEmpty from "../../DataEmpty";
 
 interface IConjugationListProps {
     wordId: number
@@ -13,38 +16,84 @@ export default function ConjugationList( props: IConjugationListProps) {
         isLoading: conjugationIsLoading,
     } = useConjugations(props.wordId)
 
+    const [modalUpdateVisible, setModalUpdateVisible] = useState(false)
+    const [modalDeleteVisible, setModalDeleteVisible] = useState(false)
 
-    if(conjugation === undefined || conjugationIsLoading) {
+    function handleUpdateConjugation(conjugationId: number) {
+        setModalUpdateVisible(true);
+    }
+
+    function handleDeleteConjugation(conjugationId: number) {
+        setModalDeleteVisible(true);
+    }
+
+    if(conjugationIsLoading) {
         return <Text>Loading...</Text>
     }
 
+    if(!conjugation){
+        return <DataEmpty message="No conjugation"/>
+    }
+
     return (
-        <Row
+        <Column
             w={'100%'}
             p={4}
             _dark={{bg: 'gray.700'}}
             _light={{bg: 'white'}}
-            justifyContent={'space-between'}
-            alignItems={'center'}
         >
-            <Column space={4}>
-                <Text fontWeight={'bold'}>Present: {conjugation.present}</Text>
-                <Text fontWeight={'bold'}>Negative: {conjugation.negative}</Text>
-                <Text fontWeight={'bold'}>Past: {conjugation.past}</Text>
-                <Text fontWeight={'bold'}>Te form: {conjugation.te_form}</Text>
-            </Column>
-            <Column space={4}>
-                <Text fontWeight={'bold'}>Potential: {conjugation.potential}</Text>
-                <Text fontWeight={'bold'}>Volitional: {conjugation.volitional}</Text>
-                <Text fontWeight={'bold'}>Causative: {conjugation.causative}</Text>
-                <Text fontWeight={'bold'}>Passive: {conjugation.passive}</Text>
-            </Column>
-            <Column space={4}>
-                <Text fontWeight={'bold'}>Causative Passive: {conjugation.causative_passive}</Text>
-                <Text fontWeight={'bold'}>Conditional: {conjugation.conditional}</Text>
-                <Text fontWeight={'bold'}>Imperative: {conjugation.imperative}</Text>
-            </Column>
+            <Row
+                justifyContent={'flex-end'}
+                alignItems={'center'}
+                space={6}
+            >
+                <Pressable
+                    onPress={() => handleUpdateConjugation(conjugation.id)}
+                >
+                    <MdEdit size={25} color="#D02C23" />
+                </Pressable>
+                <Pressable
+                    onPress={() => handleDeleteConjugation(conjugation.id)}
+                >
+                    <MdDelete size={25} color="#D02C23" />
+                </Pressable>
+            </Row>
+            <Row
+                justifyContent={'space-between'}
+                alignItems={'center'}
+            >
+                <Column space={4}>
+                    <Text fontWeight={'bold'}>Present: {conjugation.present}</Text>
+                    <Text fontWeight={'bold'}>Negative: {conjugation.negative}</Text>
+                    <Text fontWeight={'bold'}>Past: {conjugation.past}</Text>
+                    <Text fontWeight={'bold'}>Te form: {conjugation.te_form}</Text>
+                </Column>
+                <Column space={4}>
+                    <Text fontWeight={'bold'}>Potential: {conjugation.potential}</Text>
+                    <Text fontWeight={'bold'}>Volitional: {conjugation.volitional}</Text>
+                    <Text fontWeight={'bold'}>Causative: {conjugation.causative}</Text>
+                    <Text fontWeight={'bold'}>Passive: {conjugation.passive}</Text>
+                </Column>
+                <Column space={4}>
+                    <Text fontWeight={'bold'}>Causative Passive: {conjugation.causative_passive}</Text>
+                    <Text fontWeight={'bold'}>Conditional: {conjugation.conditional}</Text>
+                    <Text fontWeight={'bold'}>Imperative: {conjugation.imperative}</Text>
+                </Column>
+            </Row>
 
-        </Row>
+            <ModalDeleteConjugation
+                isOpen={modalDeleteVisible}
+                onClose={()=>setModalDeleteVisible(false)}
+                conjugationId={conjugation.id}
+                wordId={props.wordId}
+            />
+
+            <ModalUpdateConjugation
+                isOpen={modalUpdateVisible}
+                onClose={()=>setModalUpdateVisible(false)}
+                conjugationId={conjugation.id}
+                wordId={props.wordId}
+            />
+        </Column>
     )
 }
