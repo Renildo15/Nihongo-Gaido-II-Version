ae1f6a2fe60fbb33f815cf47bd872bee778f97dc
Renildo Rabil <renildorabi@ufrn.edu.br>
2024-01-21 12:25:48 -0300

    refactor: refatorando for de gramatica

---
+++ frontend/components/Select/index.tsx
@@ -0,0 +1,40 @@
+import React  from "react";
+import { FieldError, UseFormRegister } from "react-hook-form";
+import { Column, Text } from "native-base";
+import Style from "../../styles/Home.module.css"
+
+interface ISelectProps{
+    label: string;
+    name: string;
+    register: UseFormRegister<any>;
+    errors: Record<string, FieldError>;
+    options: any;
+}
+
+export default function Select({ label, name, register, errors, options }: ISelectProps) {
+    return (
+        <Column space={2}>
+            <Text
+                fontSize="md"
+                fontWeight="bold"
+            >
+                {label}
+            </Text>
+            <select
+                className={Style.inputs}
+                {...register(name, { required: true })}
+                defaultValue=""
+            >
+                <option value="" disabled hidden>Level</option>
+                {options.map((option: any) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+            {errors?.[name]?.type === "required" && (
+                <Text color={'red.500'}>{`${label} is required.`}</Text>
+            )}
+        </Column>
+    )
+}
